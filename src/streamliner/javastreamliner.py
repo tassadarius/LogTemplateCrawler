cd5c3640cbdce4724f139da15cd82290043df5b6
Michael Altenhuber <michael@altenhuber.net>
2020-06-11 13:14:18 +0200

    Added "streamliner" which unifies parameter format

---
+++ src/streamliner/javastreamliner.py
@@ -0,0 +1,92 @@
+import pandas as pd
+from tokentypes import TokenType
+from ast import literal_eval
+from typing import List
+import random
+
+
+def formalize(data: pd.DataFrame, possible_types: List[TokenType]):
+    nan_rows = data[data.iloc[:, 0].isnull()]
+    print(f'Contained {nan_rows} empty rows')
+    data = data.drop(nan_rows.index)
+    data['arguments'] = data['arguments'].apply(literal_eval)
+    data['preformat'] = data.iloc[:, 0].apply(_parse_string)
+    data['formatter_count'] = data['preformat'].apply(_count_formatters)
+    data['param_count'] = data.iloc[:, 1].apply(len)
+    data = data[data['param_count'] > 0]
+    mask = data.apply(lambda row: row['param_count'] == row['formatter_count'], axis=1)
+    data = data[mask]
+
+    output = data.apply(lambda x: _match_tokens(x['preformat'], x['arguments'], tokens=possible_types), axis=1)
+    return output.tolist()
+
+
+def _match_tokens(inp: List, params: List[str], tokens: List[TokenType]):
+    offsets = []
+    for i, token in enumerate(inp):
+        if token == '{}':
+            offsets.append(i)
+
+    possible_tokens = {}
+    for i, param in enumerate(params):
+        possible_tokens[param] = []
+        param_low = param.lower()
+        for token in tokens:
+            for feature in token.keywords:
+                if param_low.find(feature) >= 0:
+                    possible_tokens[param].append(token)
+                    break
+
+    for i, param in enumerate(params):
+        if possible_tokens[param]:
+            token = random.choice(possible_tokens[param])
+            inp[offsets[i]] = f'{{{token.name}}}'
+
+    return ' '.join(inp)
+
+
+
+
+
+def _count_formatters(inp: List[str]) -> int:
+    return inp.count('{}')
+
+
+def _parse_string(inp: str) -> List[str]:
+    output = []
+    current_tokens = ""
+    pos = 0
+    for i in range(len(inp)):
+        if pos >= len(inp):
+            break
+        c = inp[pos]
+        if c == '{':
+            if inp[pos + 1] == '{':
+                current_tokens += inp[pos: pos + 1]
+                pos += 1
+                pass
+            elif (offset := _peek(inp[pos + 1:], '}')) > 0:
+                if len(current_tokens) > 0:
+                    output.append(current_tokens)
+                output.append('{}')
+                pos += offset + 1   # +1 for normal movement
+                current_tokens = ""
+            else:
+                raise ValueError('{ opened but never closed')
+        else:
+            current_tokens += c
+            pos += 1
+    if len(current_tokens) > 0:
+        output.append(current_tokens)
+    return output
+
+
+def _peek(inp: str, end: str) -> int:
+    for i, c in enumerate(inp):
+        if c == end:
+            return i + 1
+        elif c in {' ', '\n', '\t'}:
+            pass
+        else:
+            print(f'Unexpected token {c} between format brackets {{}}')
+    return -1

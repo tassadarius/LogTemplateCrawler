8e86baacb4505549308a615f14658eed667b85d4
Michael Altenhuber <michael@altenhuber.net>
2020-08-05 00:26:36 +0200

    Add a interface class for parsing

---
+++ src/templatecrawler/parser.py
@@ -0,0 +1,28 @@
+from typing import List, Union
+import pandas as pd
+
+from templatecrawler.logparser.java import JavaParser
+
+
+class LogParser:
+
+    JAVA = 'java'
+    PYTHON = 'python'
+    CSHARP = 'csharp'
+
+    _engine_selector = {'java': JavaParser,
+                        'python': NotImplementedError,
+                        'csharp': NotImplementedError}
+
+    def __init__(self, language: str):
+        self.language = language
+        self._engine = self._engine_selector[language]
+
+    def run(self, raw_input: Union[List[str], pd.Series], framework: str):
+        engine = self._engine(framework)
+        if isinstance(raw_input, list):
+            raw_input = pd.Series(raw_input)
+        if isinstance(raw_input, pd.Series):
+            return engine.run(raw_input)
+        else:
+            raise ValueError(f'Expected type <List> or <pandas.Series> got <{type(raw_input)}> instead')

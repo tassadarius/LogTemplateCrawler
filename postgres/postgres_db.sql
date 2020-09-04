0fa72085e69e467426ae50e9957cc96b97b77f2a
Michael Altenhuber <michael@altenhuber.net>
2020-09-04 11:19:04 +0200

    Extend schema by unique constraints on repo url and cursor table

--- postgres/postgres_db.sql
+++ postgres/postgres_db.sql
@@ -17,6 +17,37 @@ SET client_min_messages = warning;
 SET row_security = off;
 
 --
+-- Name: rotate(); Type: FUNCTION; Schema: public; Owner: tassadarius
+--
+
+CREATE FUNCTION public.rotate() RETURNS trigger
+    LANGUAGE plpgsql
+    AS $$
+BEGIN
+  DELETE FROM cursor WHERE date NOT IN (SELECT date FROM cursor ORDER BY date DESC LIMIT 10); RETURN NULL;
+END;
+$$;
+
+
+ALTER FUNCTION public.rotate() OWNER TO tassadarius;
+
+SET default_tablespace = '';
+
+SET default_table_access_method = heap;
+
+--
+-- Name: cursor; Type: TABLE; Schema: public; Owner: tassadarius
+--
+
+CREATE TABLE public.cursor (
+    cursor integer,
+    date timestamp without time zone
+);
+
+
+ALTER TABLE public.cursor OWNER TO tassadarius;
+
+--
 -- Name: discarded_repositories_repo_id_seq; Type: SEQUENCE; Schema: public; Owner: tassadarius
 --
 
@@ -29,10 +60,6 @@ CREATE SEQUENCE public.discarded_repositories_repo_id_seq
 
 
 ALTER TABLE public.discarded_repositories_repo_id_seq OWNER TO tassadarius;
-
-SET default_tablespace = '';
-
-SET default_table_access_method = heap;
 
 --
 -- Name: discarded_repositories; Type: TABLE; Schema: public; Owner: tassadarius
@@ -72,7 +99,11 @@ CREATE TABLE public.repositories (
     disk_usage integer,
     license text,
     languages text[],
-    owner character varying(96)
+    owner character varying(96),
+    framework character varying(24),
+    successfully_processed boolean,
+    locked boolean DEFAULT false,
+    main_language character varying(40)
 );
 
 
@@ -106,12 +137,13 @@ ALTER SEQUENCE public.repositories_repo_id_seq OWNED BY public.repositories.repo
 
 CREATE TABLE public.templates (
     template_id integer NOT NULL,
-    repository integer NOT NULL,
+    repo_id integer NOT NULL,
     template text NOT NULL,
     crawl_date timestamp with time zone,
     framework character varying(16),
     raw text,
-    parsed text
+    parsed_template text,
+    arguments text[]
 );
 
 
@@ -162,11 +194,35 @@ ALTER TABLE ONLY public.discarded_repositories
 
 
 --
+-- Name: discarded_repositories discarded_repositories_url_key; Type: CONSTRAINT; Schema: public; Owner: tassadarius
+--
+
+ALTER TABLE ONLY public.discarded_repositories
+    ADD CONSTRAINT discarded_repositories_url_key UNIQUE (url);
+
+
+--
+-- Name: templates no_duplicates; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.templates
+    ADD CONSTRAINT no_duplicates UNIQUE (template);
+
+
+--
 -- Name: repositories repositories_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
 --
 
 ALTER TABLE ONLY public.repositories
     ADD CONSTRAINT repositories_pkey PRIMARY KEY (repo_id);
+
+
+--
+-- Name: repositories repositories_url_key; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.repositories
+    ADD CONSTRAINT repositories_url_key UNIQUE (url);
 
 
 --
@@ -185,11 +241,18 @@ CREATE UNIQUE INDEX url_idx ON public.discarded_repositories USING btree (url);
 
 
 --
+-- Name: cursor rotate_cursors; Type: TRIGGER; Schema: public; Owner: tassadarius
+--
+
+CREATE TRIGGER rotate_cursors AFTER INSERT ON public.cursor FOR EACH STATEMENT EXECUTE FUNCTION public.rotate();
+
+
+--
 -- Name: templates templates_repository_fkey; Type: FK CONSTRAINT; Schema: public; Owner: postgres
 --
 
 ALTER TABLE ONLY public.templates
-    ADD CONSTRAINT templates_repository_fkey FOREIGN KEY (repository) REFERENCES public.repositories(repo_id);
+    ADD CONSTRAINT templates_repository_fkey FOREIGN KEY (repo_id) REFERENCES public.repositories(repo_id);
 
 
 --

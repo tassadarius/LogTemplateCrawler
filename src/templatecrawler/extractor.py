d3191de532f9bab4c7b5f7eed9dee92e31441176
Michael Altenhuber <michael@altenhuber.net>
2020-09-10 00:10:32 +0200

    Adapt to be able to handle C

--- src/templatecrawler/extractor.py
+++ src/templatecrawler/extractor.py
@@ -3,6 +3,7 @@ from typing import List
 from templatecrawler.logextractor.java.log4j import log4jExtractor
 from templatecrawler.logextractor.java.slf4j import slf4jExtractor
 from templatecrawler.logextractor.java.utillogger import utilloggerExtractor
+from templatecrawler.logextractor.c.c import CExtractor
 
 
 class LogExtractor:
@@ -11,14 +12,27 @@ class LogExtractor:
     PYTHON = 'python'
     CSHARP = 'csharp',
 
-    _java_framework_selector = {'log4j': log4jExtractor,
-                                'slf4j': slf4jExtractor,
-                                'util': utilloggerExtractor,
-                                'utillogger': utilloggerExtractor,
-                                'unknown': log4jExtractor}
+    _java_framework_selector = {
+        'log4j': log4jExtractor,
+        'slf4j': slf4jExtractor,
+        'util': utilloggerExtractor,
+        'utillogger': utilloggerExtractor,
+        'unknown': log4jExtractor
+    }
+
+    # Since C doesn't support frameworks, and I just want to be sure when a mistake happens, every Java framework
+    # is replicated here.
+    _c_framework_selector = {
+        'log4j': CExtractor,
+        'slf4j': CExtractor,
+        'util': CExtractor,
+        'utillogger': CExtractor,
+        'unknown': CExtractor,
+    }
+
     _engine_selector = {'java': _java_framework_selector,
                         'python': NotImplementedError,
-                        'c': _java_framework_selector,
+                        'c': _c_framework_selector,
                         'csharp': NotImplementedError}
 
     def __init__(self, language: str, framework: str, repository: str):

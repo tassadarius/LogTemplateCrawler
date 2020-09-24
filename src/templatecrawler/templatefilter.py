9b5e8a09be27e346b85ae54b4759e4234164d7c5
Michael Altenhuber <michael@altenhuber.net>
2020-09-24 12:19:27 +0200

    Make the filter simpler and better, by avoiding complicated regexes

--- src/templatecrawler/templatefilter.py
+++ src/templatecrawler/templatefilter.py
@@ -2,15 +2,14 @@ import re
 import pandas as pd
 from typing import Union, List
 
-re_length = re.compile(r'^.{10,}$')
-re_characters = re.compile(r'( *? [a-zA-Z] *?)')
-re_char = re.compile('[a-zA-Z]')
+re_length = re.compile(r'^.{10,}$')                     # Has to have a certain length
+re_char = re.compile('^[^a-wyzA-WYZ]+$')                # Contains no letters, except x for 0x hex escape
 
 
 def find_valid(data: Union[pd.Series, List[str]]) -> pd.Series:
     if isinstance(data, List):
         data = pd.Series(data)
 
-    mask = data.str.count(re_length).astype(bool)
-    mask |= data.str.count(re_char) > 20
+    mask = data.apply(len) > 12
+    mask |= ~data.str.match(re_char)
     return mask

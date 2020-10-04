4a25889a6c09f1b41518f66f9ef323a0427a79b0
Michael Altenhuber <michael@altenhuber.net>
2020-10-04 09:37:54 +0200

    Update filters

--- src/templatecrawler/templatefilter.py
+++ src/templatecrawler/templatefilter.py
@@ -2,14 +2,20 @@ import re
 import pandas as pd
 from typing import Union, List
 
-re_length = re.compile(r'^.{10,}$')                     # Has to have a certain length
-re_char = re.compile('^[^a-wyzA-WYZ]+$')                # Contains no letters, except x for 0x hex escape
+re_length = re.compile(r'^.{10,}$')
+re_char = re.compile(r'^[^a-wyzA-WYZ]+$')
+
+re_keyword_beginnings = re.compile(r'^\s*(static|#include|#define|#if|#endif)')
+re_comment = re.compile(r'^\s*(//|\*)')
 
 
 def find_valid(data: Union[pd.Series, List[str]]) -> pd.Series:
     if isinstance(data, List):
         data = pd.Series(data)
 
-    mask = data.apply(len) > 12
-    mask |= ~data.str.match(re_char)
+    mask = data.apply(len) > 14                       # Has to be at least n characters long
+    mask |= ~data.str.match(re_char)                  # Has to contain letters
+    mask |= ~data.str.match(re_keyword_beginnings)    # Exclude certain keywords, which may come from a parsing problem
+    mask |= ~data.str.match(re_comment)               # Exclude comments, which may come from a parsing problem
+    mask |= ~ data.str.count('{}') > 12               # Exclude templates with more than 12 placeholders
     return mask
